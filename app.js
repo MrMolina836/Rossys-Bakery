54a74a65683d1549ae91f371e5f1e5b3bbe06932
MrMolina836 <robertomolina8.361110@gmail.com>
2023-10-09 12:26:42 -0400

    Gallery Fliter is complete and responsive

---
+++ app.js
@@ -0,0 +1,24 @@
+let list = document.querySelectorAll('.list');
+let item__box = document.querySelectorAll('.item__box');
+
+for(let i = 0; i<list.length; i++){
+    list[i].addEventListener('click', function(){
+        for(let j = 0; j<list.length; j++){
+            list[j].classList.remove('active');
+        }
+        this.classList.add('active');
+
+        let dataFilter = this.getAttribute('data-filter');
+
+        for(let k = 0; k<item__box.length; k++){
+            item__box[k].classList.remove('active');
+            item__box[k].classList.add('hide');
+
+            if(item__box[k].getAttribute('data-item') == dataFilter || 
+            dataFilter == "all"){
+                item__box[k].classList.remove('hide');
+                item__box[k].classList.add('active');
+            }
+        }
+    })
+}
